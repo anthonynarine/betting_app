2ec1ee6c9fa78d91571396386ef36d1adeb07962
anthonynarine <anarine83@gmail.com>
2023-10-10 22:37:58 -0400

    added the Members model did not migrate.  My update Group model to League

--- backend/betting_project/api/models.py
+++ backend/betting_project/api/models.py
@@ -1,38 +1,50 @@
+from django.conf import settings
 from django.db import models
 
-
-
-
-
-
 class Group(models.Model):
-    name = models.CharField(max_length=32, null=False, unique=False)
-    location = models.CharField(max_length=32, null=False)
-    description = models.CharField(max_length=256, null=False)
+    name = models.CharField(max_length=32, unique=True)
+    location = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        unique_together = ("name", "location")
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Member', related_name='groups')
 
     def __str__(self):
         return f"{self.name} - {self.location}"
 
 
 class Event(models.Model):
-    team1 = models.CharField(max_length=32, blank=False)
-    team2 = models.CharField(max_length=32, blank=False)
-    time = models.DateTimeField(null=False, blank=False)
-    score1 = models.IntegerField(null=True, blank=True)
-    score2 = models.IntegerField(null=True, blank=True)
+    team1 = models.CharField(max_length=32)
+    team2 = models.CharField(max_length=32)
+    time = models.DateTimeField()
+    team1_score = models.IntegerField(null=True, blank=True) 
+    team2_score = models.IntegerField(null=True, blank=True)  
     group = models.ForeignKey(Group, related_name="events", on_delete=models.CASCADE)
     participants = models.ManyToManyField(
-       "users.CustomUser", related_name="attended_events", blank=True
+       settings.AUTH_USER_MODEL, related_name="attended_events", blank=True
     )
 
     def __str__(self):
         return f"{self.team1} - {self.team2}"
 
+
+class Member(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    admin = models.BooleanField(default=False)  
+    joined_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'group')
+        ordering = ['joined_at']
+
+    def __str__(self):
+        return f"{self.user.username} in {self.group.name}"
+
+    
+    
+    
+    
 
 # CustomUser:
 # - Attends -> Many Events (Event's attendees field)
