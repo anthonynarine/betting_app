de1f0bbec90c82c0277ef388b63568749c517755
anthonynarine <anarine83@gmail.com>
2023-11-23 12:21:55 -0500

    create a new dir in api app for testing + create a method for setting up a user + group + event and tested an event createion

---
+++ backend/betting_project/api/tests/models/test_bets.py
@@ -0,0 +1,35 @@
+import time
+from django.test import TestCase
+from api.models import Bet, Event, Group
+from django.contrib.auth import get_user_model 
+
+class BetModelTestCase(TestCase):
+    def setUp(self):
+        #set up data needed for test
+        User = get_user_model() # get the user model
+        self.user = User.objects.create(username="testuser")
+        self.group = Group.objects.create(name="Test Group")
+        self.event = Event.objects.create(
+            team1="Team 1",
+            team2="Team 2",
+            team1_score=None,
+            team2_score=None,
+            time="2024-11-23 12:00",
+            organizer=None,
+            group=self.group,
+            is_archived=False,
+        )
+        self.bet = Bet.objects.create(
+            user=self.user,
+            event=self.event,
+            team_choice="Team 1",
+            bet_type="Win",
+            points=50
+        )
+        
+    def test_bet_createion(self):
+        # test if a Bet object was created successfully
+        bet = Bet.objects.get(id=self.bet.id)
+        self.assertEqual(bet.team_choice, "Team 1")
+        self.assertEqual(bet.bet_type, "Win")
+        self.assertEqual(bet.points, 50)
