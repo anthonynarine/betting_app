5a88f05ef6144162f0d1a2a8a8b2a674d4771239
anthonynarine <anarine83@gmail.com>
2023-12-05 12:49:55 -0500

    overode update and destroy EventViewSet method to rais error if user is not authenticated

--- backend/betting_project/api/views/event_views.py
+++ backend/betting_project/api/views/event_views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
 class EventViewset(viewsets.ModelViewSet):
     """
@@ -22,6 +23,18 @@ class EventViewset(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         print("Received data for Event creation:", request.data)
         return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        event = self.get_object()
+        if request.user != event.organizer:
+            raise PermissionDenied 
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        event = self.get_object()
+        if request.user != event.organizer:
+            raise PermissionDenied
+        return super().destroy(request, *args, **kwargs)
 
     
     @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
