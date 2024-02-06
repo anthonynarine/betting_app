17e8a3ebe154d786c3de1b03119994f2d04ff5f7
Mitra Singh <mitra.coder@gmail.com>
2024-02-05 23:09:24 -0500

    added new card UI

--- frontend/src/pages/home/main/GroupCardTest.jsx
+++ frontend/src/pages/home/main/GroupCardTest.jsx
@@ -11,6 +11,8 @@ export default function MultiActionAreaCard({ group }) {
     const cardImageURL = "https://npr.brightspotcdn.com/dims4/default/cbb3940/2147483647/strip/true/crop/6016x4016+0+0/resize/880x587!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Fcd%2Fbb%2F83b9caa44455a5b528e89edbc1cd%2Fvecteezy-man-at-a-football-match-follows-the-odds-and-the-current-25482396-665.jpg"
     const avatarURL = "https://media.npr.org/assets/img/2013/09/27/m_017_dja_06081crop_-83201a29-fae8-e211-86ee-4040e990bee0-_lg-d5be7edbb70301240aefa4f25872b9abbc7bdd29.jpg"
     const groupNameUppercase = group.name.toUpperCase();
+
+    // 
     return (
         <Card sx={{ maxWidth: 250, paddingBottom: .5 }}>
             <CardActionArea>
