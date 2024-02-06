fb397fc601cb0a033da4cf8c1bd9c2fe98abae5f
anthonynarine <anarine83@gmail.com>
2024-02-05 19:04:57 -0500

    Updated GroupDataProvider had to seperate the logic to fetch and store a single group and all groups in GroupDataProvider Also updated MyBets tab with a list of bets

--- frontend/src/pages/events/bets/BetList.jsx
+++ frontend/src/pages/events/bets/BetList.jsx
@@ -5,9 +5,7 @@ import {
   Card,
   CardContent,
   Typography,
-  List,
-  ListItem,
-  ListItemText,
+  Divider,
 } from '@mui/material';
 import { useBetData } from '../../../context/bet/BetDataProvider';
 import { useUserServices } from '../../../context/user/UserContext';
@@ -16,57 +14,50 @@ const BetList = () => {
   const { bets } = useBetData();
   const { userData } = useUserServices();
 
-
-  const listItemStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    backgroundColor: '#f5f5f5', 
-    padding: '8px 16px',
-    margin: '8px 0',
-    borderRadius: '4px',
-  };
-
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
       <Grid container spacing={4} justifyContent="center">
         {bets && bets.length > 0 ? (
           bets.map((bet) => {
-      
-            const numberOfParticipants = bet.event.participants_bets_and_winnings.participants_info.length;
+            // Extract necessary data from the bet object
             const currentUserBetInfo = bet.event.participants_bets_and_winnings.participants_info.find(participant => participant.user === userData.username);
             const totalWinnablePool = currentUserBetInfo ? currentUserBetInfo.total_winnable_pool : 0;
             const potentialWinning = currentUserBetInfo ? currentUserBetInfo.potential_winning : 0;
 
             return (
-              <Grid item key={bet.id} xs={12} sm={6} md={4}>
-                <Card  sx={{ minWidth: 275, maxWidth: 345, m: 'auto', backgroundColor: 'black' }}>
+              <Grid item key={bet.id} xs={12}>
+                <Card sx={{ m: 'auto', bgcolor: 'background.paper', boxShadow: 3 }}>
                   <CardContent>
-                    <Typography variant="h5" component="div" gutterBottom sx={{color: "white"}}>
-                      {bet.event.group.name}
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="white">
-                      {bet.event.team1} vs {bet.event.team2}
-                    </Typography>
-                    <List sx={{ padding: 0 }}>
-                      <ListItem sx={listItemStyle}>
-                        <ListItemText primary={`Bet on: ${bet.chosen_team_name}`} />
-                      </ListItem>
-                      <ListItem sx={listItemStyle}>
-                        <ListItemText primary={`Amount: $${bet.bet_amount}`} />
-                      </ListItem>
-                      <ListItem sx={listItemStyle}>
-                        <ListItemText primary={`Type: ${bet.bet_type}`} />
-                      </ListItem>
-                      <ListItem sx={listItemStyle}>
-                        <ListItemText primary={`Participants: ${numberOfParticipants}`} />
-                      </ListItem>
-                      <ListItem sx={listItemStyle}>
-                        <ListItemText primary={`Total Winnable Pool: $${totalWinnablePool}`} />
-                      </ListItem>
-                      <ListItem sx={listItemStyle}>
-                        <ListItemText primary={`Your Potential Winning: $${potentialWinning}`} />
-                      </ListItem>
-                    </List>
+                    {/* Header with Group Name and Event Matchup */}
+                    <Grid container alignItems="center" justifyContent="flex-start">
+                      <Typography variant="subtitle1" sx={{ fontWeight: 'bold', color: 'text.primary', display: 'inline' }}>
+                        {bet.event.group.name}
+                      </Typography>
+                      <Typography variant="subtitle1" sx={{ flexGrow: 1, textAlign: 'center', color: 'text.secondary', display: 'inline' }}>
+                        {bet.event.team1} vs {bet.event.team2}
+                      </Typography>
+                    </Grid>
+
+                    <Divider sx={{ my: 2 }} />
+
+                    {/* Bet Details */}
+                    <Grid container spacing={2} alignItems="center">
+                      <Grid item xs={12} sm={6}>
+                        <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+                          Bet on: <span style={{ fontWeight: 'bold' }}>{bet.chosen_team_name}</span> - Amount: <span style={{ fontWeight: 'bold' }}>${bet.bet_amount}</span>
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+                          Total Pool: <span style={{ fontWeight: 'bold' }}>${totalWinnablePool}</span> - Participants: <span style={{ fontWeight: 'bold' }}>{bet.event.participants_bets_and_winnings.participants_info.length}</span>
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography variant="body1" sx={{ color: 'text.secondary', mt: 1 }}>
+                          Potential Winning: <span style={{ fontWeight: 'bold' }}>${potentialWinning}</span>
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </CardContent>
                 </Card>
               </Grid>
@@ -83,6 +74,7 @@ const BetList = () => {
 };
 
 export default BetList;
+
 
 
 
