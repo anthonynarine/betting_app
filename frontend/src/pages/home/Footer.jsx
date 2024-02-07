dfc14dee1413cfa37bbfb9a269258995f09863bc
Mitra Singh <mitra.coder@gmail.com>
2024-02-06 23:49:14 -0500

    reduced height of footer

--- frontend/src/pages/home/Footer.jsx
+++ frontend/src/pages/home/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      height: 90, // Adjust the height as needed
+      height: 50, // Adjust the height as needed
       backgroundColor: '#0A0A0A', // Or any color that fits your theme
       color: '#fff',
       mt: 'auto', // This helps push the footer to the bottom
